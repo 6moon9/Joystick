44ee610964b52835a1ca0b5343fda0a34df1c8d4
SimonPucheu <104694344+SimonPucheu@users.noreply.github.com>
2023-01-21 09:51:51 +0100

    Add the reverse, margin and rest parameters

--- src/Joystick.cpp
+++ src/Joystick.cpp
@@ -5,9 +5,16 @@
 /**
  * Setup the joystick
  */
-Joystick::Joystick(uint8_t xPin, uint8_t yPin, uint8_t cPin, bool xReversed = false, bool yReversed = false)
+Joystick::Joystick(uint8_t xPin, uint8_t yPin, uint8_t cPin, bool xReversed, bool yReversed, int xRest, int yRest, int xMargin, int yMargin)
 {
-    x = Potentiometer(xPin, xReversed);
-    y = Potentiometer(yPin, yReversed);
+    x = Potentiometer(xPin, xReversed, xRest, xMargin);
+    y = Potentiometer(yPin, yReversed, yRest, yMargin);
+    clck = Button(cPin);
+}
+
+Joystick::Joystick(uint8_t xPin, uint8_t yPin, uint8_t cPin)
+{
+    x = Potentiometer(xPin);
+    y = Potentiometer(yPin);
     clck = Button(cPin);
 }
